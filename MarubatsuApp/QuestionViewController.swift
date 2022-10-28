c6976f3b02cb1b780dee2dda36f83790469b0e10
“wuuud” <syrdiy@gmail.com>
2022-10-28 19:25:32 +0900

    マルバツアプリ 完成(仮)

---
+++ MarubatsuApp/QuestionViewController.swift
@@ -0,0 +1,81 @@
+//
+//  QuestionViewController.swift
+//  MarubatsuApp
+//
+//  Created by H M on 2022/10/23.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    //1.初期値
+    var questions: [[String: Any]] = []
+    var userDefaults = UserDefaults.standard
+    //2.紐付け
+    //トップ画面に戻る
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: false, completion: nil)
+    }
+    @IBOutlet weak var questionMakeText: UITextField!
+    @IBAction func storeButton(_ sender: UIButton) {
+        if questionMakeText.text != "" {
+            //真偽値の初期値
+            var boolAnswer: Bool
+            //https://ios-docs.dev/uisegmentedcontrol/
+            switch selectSegmentedControl.selectedSegmentIndex {
+            case 0:
+                boolAnswer = false
+            case 1:
+                boolAnswer = true
+            default:
+                boolAnswer = false
+            }
+            questions = userDefaults.object(forKey: "questions") as! [[String: Any]]
+            questions.append(["question": questionMakeText.text!, "answer": boolAnswer])
+            userDefaults.set(questions, forKey: "questions")
+            showAlert(message:  "登録完了")
+            questionMakeText.text = ""
+            print(questions)
+            
+        } else {
+            showAlert(message: "問題文を入力してください")
+        }
+    }
+    @IBAction func allClearButton(_ sender: UIButton) {
+        questions.removeAll()
+        // 追加：削除した内容を保存
+        userDefaults.set(questions, forKey: "questions")
+        //tableView更新は不要 ページ再読み込みしない tableView.reloadData()
+        showAlert(message: "問題を全て削除しました")
+        print(questions)
+    }
+    //segmented https://qiita.com/rea_sna/items/7e472af2ce8d03831b55
+    @IBOutlet weak var selectSegmentedControl: UISegmentedControl!
+    @IBAction func tappedSegmentedControl(_ sender: UISegmentedControl) {
+    }
+    
+    //3.自動
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionMakeText.placeholder = "問題を入力してください"
+        //       UserDefaultsからすでに入力されている質問と答えをquestionsに読み込む
+        if userDefaults.object(forKey: "questions") != nil{
+            questions = userDefaults.object(forKey: "questions") as! [[String: Any]]
+        }
+        
+    }
+    
+    //テキストフィールドに最初からフォーカスが表示される
+    override func viewWillAppear(_ animated: Bool) {
+        questionMakeText.becomeFirstResponder()
+    }
+    
+    //4.関数
+    //①アラート 決まり文句checkAnswerに追加showAlert(message: "正解！")
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
+}
--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -8,59 +8,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //1.初期設定
+    //①表示中の問題番号を格納
+    //var currentQuestionNum: Int = 0 型推論できるので下記の書き方でもOK
+    var currentQuestionNum = 0
+    //②問題のリスト配列  配列[]の中に連想配列[]
+    var questions: [[String: Any]] = []
+    var userDefaults = UserDefaults.standard
+    //問題ない場合にボタン操作不能
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var yesButton: UIButton!
+    //2.紐付け
     @IBAction func tappedNoButton(_ sender: UIButton) {
         checkAnswer(yourAnswer: false)
     }
     @IBAction func tappedYesButton(_ sender: UIButton) {
-        checkAnswer(yourAnswer: true)
+        if questions.isEmpty == false {
+            checkAnswer(yourAnswer: true)
+        }
     }
-    @IBOutlet weak var questionLabel: UILabel!
-    // 変数1 表示中の問題番号を格納
-    //var currentQuestionNum: Int = 0
-    //型推論できるので下記の書き方でもOK
-    var currentQuestionNum = 0
-    //変数2 問題のリストを辞書の配列  配列[]の中に連想配列[]
-    let questions: [[String: Any]] = [
-        //        配列の中にキーとバリュー
-        [
-            "question": "iPhoneアプリを開発する統合環境はZcodeである",
-            "answer": false
-        ],
-        [
-            "question": "Xcode画面の右側にはユーティリティーズがある",
-            "answer": true
-        ],
-        [
-            "question": "UILabelは文字列を表示する際に利用する",
-            "answer": true
-        ]
-    ]
     
-    //問題表示用の関数を作成
+    func notenabledButton() {
+        if questions.isEmpty {
+            noButton.isEnabled = false
+            yesButton.isEnabled = false
+        } else {
+            noButton.isEnabled = true
+            yesButton.isEnabled = true
+        }
+    }
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    //3.override
+    //問題表示用の関数を呼び出し
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //A.UserDefaultsからすでに入力されている質問と答えをquestionsに読み込む
+        if userDefaults.object(forKey: "questions") != nil{
+            questions = userDefaults.object(forKey: "questions") as! [[String: Any]]
+        }
+    }
+    //同じ処理を何度も繰り返し行えるように
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //テキストフィールドに最初からフォーカスが表示される
+        questionLabel.becomeFirstResponder()
+        //UserDefaultsからすでに入力されている質問と答えをquestionsに読み込む
+        if userDefaults.object(forKey: "questions") != nil{
+            questions = userDefaults.object(forKey: "questions") as! [[String: Any]]
+        }
+        //ページを更新 tableView.reloadData()は使えない。
+        showQuestion()
+        notenabledButton()
+    }
+    
+    //4.独自の関数
+    //4−1.Aciton
+    //4-2.func
+    //①問題表示用
     func showQuestion() {
         // 1つだけ取得してくる
         //例) var currentQuestionNum = 0 なら [
         //        "qusetion": "iPhoneアプリを開発する統合環境はZcodeである",
         //        "answer": false
         //    ]がquestionに入る
-        let question = questions[currentQuestionNum]
-        
-        //if letオプショナルバインディング でnulじゃないか確認後
-        if let que = question["question"] as? String {
-            questionLabel.text = que
+        if questions.isEmpty {
+            questionLabel.text = "問題がありません。新しい問題を作成してください。"
+        } else {
+            let question = questions[currentQuestionNum]
+            //if letオプショナルバインディング でnulじゃないか確認後
+            if let que = question["question"] as? String {
+                questionLabel.text = que
+            }
         }
     }
-    
-    //問題表示用の関数を呼び出し
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        showQuestion()
-    }
-    
-    //回答したときの関数 回答をチェックする関数
-    // 正解なら次の問題を表示します
+    //②回答時 回答をチェックする関数
     //yourAnswer: Boolでtrueかfalse 関数内で正解のボタンが押されたか判定するために引数を定義
     //引数といい、関数が呼び出された際に呼び出し元から渡してもらう値、
     //つまり関数checkAnswerが呼び出されたときにyourAnswer: true or falseを入れてね
@@ -76,8 +99,9 @@ class ViewController: UIViewController {
                 showAlert(message: "正解！")
             } else {
                 // 不正解
-                showAlert(message: "不正解")
+                showAlert(message: "ざんねん・・不正解")
             }
+            //if let ans = question["answer"] as? Boolのelse {
         } else {
             print("答えが入ってません")
             //これで終わり
@@ -85,17 +109,19 @@ class ViewController: UIViewController {
         }
         //エラー解消fatal error: Index out of range
         //currentQuestionNumの値が問題数以上だったら最初の問題に戻す
-                if currentQuestionNum >= questions.count {
-                    currentQuestionNum = 0
-                }
+        if currentQuestionNum >= questions.count {
+            currentQuestionNum = 0
+        }
+        //エラーDictionaryの中身がなければhttps://qiita.com/Saayaman/items/e82a09f93bb91409e446
         // 問題を表示    正解であれば次の問題が、不正解であれば同じ問題が再表示
         showQuestion()
     }
-    // 正解か不正解かアラートを表示する関数   関数・決まり文句→表示のためにfunc checkAnswerに追加showAlert(message: "正解！")
+    //③アラート 正解か不正解かアラート 決まり文句checkAnswerに追加showAlert(message: "正解！")
     func showAlert(message: String) {
-            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
-            let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
-            alert.addAction(close)
-            present(alert, animated: true, completion: nil)
-        }
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
+    }
 }
+
